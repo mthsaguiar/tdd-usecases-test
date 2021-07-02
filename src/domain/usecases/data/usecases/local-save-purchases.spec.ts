fa517936b4083f69234024aead8665526b6f69fb
Matheus Aguiar <matheusaguiar.vasta@gmail.com>
2021-07-02 17:14:00 -0300

    ensure LocalSavePurchases does not delete cache upon creation

---
+++ src/domain/usecases/data/usecases/local-save-purchases.spec.ts
@@ -0,0 +1,23 @@
+
+
+class LocalSavePurchases{
+    constructor(private readonly cacheStore: CacheStore){
+
+    }
+}
+
+interface CacheStore {
+}
+
+class CacheStoreSpy implements CacheStore {
+    deleteCallsCount = 0;
+}
+
+//sut => system under test
+describe('LocalSavePurchases', ()=>{
+   test('Should not delete cache on sut.init', ()=>{
+        const cacheStore = new CacheStoreSpy()
+        new LocalSavePurchases(cacheStore)
+        expect(cacheStore.deleteCallsCount).toBe(0)
+   })
+})
