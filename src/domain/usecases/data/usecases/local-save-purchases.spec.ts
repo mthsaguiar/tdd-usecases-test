0806e6cb6062a7c904c2e4ae05bde2e35e60c461
Matheus Aguiar <matheusaguiar.vasta@gmail.com>
2021-07-02 17:21:58 -0300

    ensure LocalSavePurchases delete old cache on save

--- src/domain/usecases/data/usecases/local-save-purchases.spec.ts
+++ src/domain/usecases/data/usecases/local-save-purchases.spec.ts
@@ -1,16 +1,23 @@
 
 
 class LocalSavePurchases{
-    constructor(private readonly cacheStore: CacheStore){
+    constructor(private readonly cacheStore: CacheStore){}
 
+    async save(): Promise<void>{
+        this.cacheStore.delete()
     }
 }
 
 interface CacheStore {
+    delete: () => void
 }
 
 class CacheStoreSpy implements CacheStore {
     deleteCallsCount = 0;
+
+    delete(): void{
+        this.deleteCallsCount++;
+    }
 }
 
 //sut => system under test
@@ -20,4 +27,11 @@ describe('LocalSavePurchases', ()=>{
         new LocalSavePurchases(cacheStore)
         expect(cacheStore.deleteCallsCount).toBe(0)
    })
+
+   test('Should delete old cache on sut.save ', async ()=>{
+    const cacheStore = new CacheStoreSpy()
+    const sut = new LocalSavePurchases(cacheStore)
+    await sut.save()
+    expect(cacheStore.deleteCallsCount).toBe(1)
+    })
 })
