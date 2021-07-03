5120f7d9e3bda9406da0068e0cb5be5512fcf712
Matheus Aguiar <matheusaguiar.vasta@gmail.com>
2021-07-03 17:30:50 -0300

    ensure LocalLoadPurchases will not delete cache upon creation

--- src/data/usecases/load-purchases/local-load-purchases.spec.ts
+++ src/data/usecases/load-purchases/local-load-purchases.spec.ts
@@ -1,5 +1,5 @@
-import { mockPurchases, CacheStoreSpy } from '@/data/tests'
-import { LocalLoadPurchases } from './local-load-purchases'
+import { LocalLoadPurchases } from '@/data/usecases'
+import {  CacheStoreSpy } from '@/data/tests'
 
 
 
@@ -22,35 +22,4 @@ describe('LocalLoadPurchases', ()=>{
         const {cacheStore} = makeSut();
         expect(cacheStore.actions).toEqual([]);
    })
-
-
-    test('Should not insert new Cache if delete fails', async ()=>{
-        const { cacheStore, sut } = makeSut();
-        cacheStore.siulateDeleteError();
-        const promise = sut.save(mockPurchases());
-        expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.delete]);
-        await expect(promise).rejects.toThrow();
-    })
-
-    test('Should insert new Cache if delete succeeds', async ()=>{
-        const timestamp = new Date()
-        const { cacheStore, sut } = makeSut();
-        const purchases = mockPurchases();
-        const promise = sut.save(purchases);
-        expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.delete, CacheStoreSpy.Action.insert]);
-        expect(cacheStore.deleteKey).toBe('purchases');
-        expect(cacheStore.insertKey).toBe('purchases');
-        expect(cacheStore.insertValues).toEqual({
-            timestamp,
-            value: purchases
-        });
-        await expect(promise).resolves.toBeFalsy()
-    })
-    test('Should throw if insert throws', async ()=>{
-        const { cacheStore, sut } = makeSut();
-        cacheStore.siulateInsertError();
-        const promise = sut.save(mockPurchases());
-        expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.delete, CacheStoreSpy.Action.insert])
-        await expect(promise).rejects.toThrow();
-    })
 })
---
+++ src/data/usecases/load-purchases/local-save-purchases.spec.ts
@@ -0,0 +1,56 @@
+import { LocalLoadPurchases } from '@/data/usecases'
+import { mockPurchases, CacheStoreSpy } from '@/data/tests'
+
+
+
+type SutTypes = {
+    sut: LocalLoadPurchases
+    cacheStore: CacheStoreSpy
+}
+
+const makeSut = (timestamp= new Date()): SutTypes =>{
+    const cacheStore = new CacheStoreSpy()
+    const sut = new LocalLoadPurchases(cacheStore, timestamp)
+    return {
+        sut,
+        cacheStore
+    }
+}
+//sut => system under test
+describe('LocalLoadPurchases', ()=>{
+   test('Should not delete or insert cache on sut.init', ()=>{
+        const {cacheStore} = makeSut();
+        expect(cacheStore.actions).toEqual([]);
+   })
+
+
+    test('Should not insert new Cache if delete fails', async ()=>{
+        const { cacheStore, sut } = makeSut();
+        cacheStore.siulateDeleteError();
+        const promise = sut.save(mockPurchases());
+        expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.delete]);
+        await expect(promise).rejects.toThrow();
+    })
+
+    test('Should insert new Cache if delete succeeds', async ()=>{
+        const timestamp = new Date()
+        const { cacheStore, sut } = makeSut();
+        const purchases = mockPurchases();
+        const promise = sut.save(purchases);
+        expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.delete, CacheStoreSpy.Action.insert]);
+        expect(cacheStore.deleteKey).toBe('purchases');
+        expect(cacheStore.insertKey).toBe('purchases');
+        expect(cacheStore.insertValues).toEqual({
+            timestamp,
+            value: purchases
+        });
+        await expect(promise).resolves.toBeFalsy()
+    })
+    test('Should throw if insert throws', async ()=>{
+        const { cacheStore, sut } = makeSut();
+        cacheStore.siulateInsertError();
+        const promise = sut.save(mockPurchases());
+        expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.delete, CacheStoreSpy.Action.insert])
+        await expect(promise).rejects.toThrow();
+    })
+})
